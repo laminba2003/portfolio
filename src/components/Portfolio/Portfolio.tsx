a1829ad9c4013c6e82c9d30aea46c3b30af7fd65
Papa Babacar BA <babacarba.moon@gmail.com>
2020-12-12 13:44:33 +0000

    Some adjustements

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -6,7 +6,7 @@ const Portfolio = () => {
           <img className="object-cover w-64 h-64" src="/Shopteamize.jpg" alt="Shopteamize" loading="lazy" />
           <a className="absolute top-0 flex flex-col items-center justify-center w-full h-full p-4 text-center bg-gray-700 bg-opacity-70 hover:bg-opacity-50">
             <h2 className="text-pink-500 font-inter">Shopteamize</h2>
-            <p className="text-pink-100 font-inter">
+            <p className="text-purple-100 font-inter">
               E-commerce marketplace
               <br />➔ React ➔ Redux ➔ NextJS ➔ Braintree
             </p>
@@ -36,7 +36,7 @@ const Portfolio = () => {
           <img className="object-cover w-64 h-64" src="/ambiance studio.jpg" alt="Ambiance Studio" loading="lazy" />
           <a className="absolute top-0 flex flex-col items-center justify-center w-full h-full p-4 text-center bg-gray-700 bg-opacity-80 hover:bg-opacity-70">
             <h2 className="text-pink-500 font-inter">Ambiance Studio</h2>
-            <p className="text-pink-100 font-inter">
+            <p className="text-purple-100 font-inter">
               Online music studio
               <br />➔ React ➔ Redux ➔ Webaudio ➔ WebAssembly
             </p>
