6e034338066ba79616d2eb3bc76f7420b2ce2e27
Papa Babacar BA <babacarba.moon@gmail.com>
2020-12-12 13:41:41 +0000

    Some adjustements

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -5,7 +5,7 @@ const Portfolio = () => {
         <div className="relative">
           <img className="object-cover w-64 h-64" src="/Shopteamize.jpg" alt="Shopteamize" loading="lazy" />
           <a className="absolute top-0 flex flex-col items-center justify-center w-full h-full p-4 text-center bg-gray-700 bg-opacity-70 hover:bg-opacity-50">
-            <h2 className="text-pink-100 font-inter">Shopteamize</h2>
+            <h2 className="text-pink-500 font-inter">Shopteamize</h2>
             <p className="text-pink-100 font-inter">
               E-commerce marketplace
               <br />➔ React ➔ Redux ➔ NextJS ➔ Braintree
@@ -15,7 +15,7 @@ const Portfolio = () => {
         <div className="relative">
           <img className="object-cover w-64 h-64" src="/livefoot.jpg" alt="Livefoot" loading="lazy" />
           <a className="absolute top-0 flex flex-col items-center justify-center w-full h-full p-4 text-center bg-gray-700 bg-opacity-80 hover:bg-opacity-70">
-            <h2 className="text-purple-100 font-inter">Livefoot</h2>
+            <h2 className="text-pink-500 font-inter">Livefoot</h2>
             <p className="text-purple-100 font-inter">
               Realtime football matches results
               <br />➔ React ➔ Redux ➔ GatbyJS ➔ TailwindCSS
@@ -25,7 +25,7 @@ const Portfolio = () => {
         <div className="relative">
           <img className="object-cover w-64 h-64" src="/findpairs.jpg" alt="Find pairs" loading="lazy" />
           <a className="absolute top-0 flex flex-col items-center justify-center w-full h-full p-4 text-center bg-gray-700 bg-opacity-80 hover:bg-opacity-70">
-            <h2 className="text-purple-100 font-inter">Find pairs</h2>
+            <h2 className="text-pink-500 font-inter">Find pairs</h2>
             <p className="text-purple-100 font-inter">
               Game of finding pairs
               <br />➔ React ➔ Redux ➔ NextJS ➔ Styled components
@@ -35,7 +35,7 @@ const Portfolio = () => {
         <div className="relative">
           <img className="object-cover w-64 h-64" src="/ambiance studio.jpg" alt="Ambiance Studio" loading="lazy" />
           <a className="absolute top-0 flex flex-col items-center justify-center w-full h-full p-4 text-center bg-gray-700 bg-opacity-80 hover:bg-opacity-70">
-            <h2 className="text-pink-100 font-inter">Ambiance Studio</h2>
+            <h2 className="text-pink-500 font-inter">Ambiance Studio</h2>
             <p className="text-pink-100 font-inter">
               Online music studio
               <br />➔ React ➔ Redux ➔ Webaudio ➔ WebAssembly
